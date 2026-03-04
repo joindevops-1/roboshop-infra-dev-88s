d94deb76af37ab64e13157ea51ef3af3ab669872
Sivakumar Reddy M <info@joindevops.com>
2026-03-04 23:20:14 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -80,6 +80,88 @@ resource "terraform_data" "redis" {
   }
 }
 
+resource "aws_instance" "mysql" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.mysql_sg_id]
+    subnet_id = local.database_subnet_id
+
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-mysql"
+        }
+    )
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = local.ssh_credentials["ssh_user"]
+    password = local.ssh_credentials["ssh_password"]
+    host     = aws_instance.mysql.private_ip
+  }
+
+  # terraform copies this file to mysql server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        # "sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh mysql dev"
+    ]
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.rabbitmq_sg_id]
+    subnet_id = local.database_subnet_id
+
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-rabbitmq"
+        }
+    )
+}
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = local.ssh_credentials["ssh_user"]
+    password = local.ssh_credentials["ssh_password"]
+    host     = aws_instance.rabbitmq.private_ip
+  }
+
+  # terraform copies this file to rabbitmq server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        # "sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh rabbitmq dev"
+    ]
+  }
+}
+
 resource "aws_route53_record" "mongodb" {
   zone_id = var.zone_id
   name    = "mongodb-${var.environment}.${var.domain_name}" # mongodb-dev.daws86s.fun
@@ -95,5 +177,23 @@ resource "aws_route53_record" "redis" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = var.zone_id
+  name    = "mysql-${var.environment}.${var.domain_name}" # mysql-dev.daws86s.fun
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name    = "rabbitmq-${var.environment}.${var.domain_name}" # rabbitmq-dev.daws86s.fun
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
   allow_overwrite = true
 }
