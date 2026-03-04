3346fbeef92636afd294426378620b5dbe76824c
Sivakumar Reddy M <info@joindevops.com>
2026-03-04 15:02:47 +0530

    terraform

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "bastion" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id
-    iam_instance_profile = "BastionTerraformAdmin"
+    iam_instance_profile = aws_iam_instance_profile.bastion.name
     # need more for terraform
     root_block_device {
         volume_size = 50
@@ -18,6 +18,44 @@ resource "aws_instance" "bastion" {
             Name = "${var.project}-${var.environment}-bastion"
         }
     )
+}
+
+resource "aws_iam_role" "bastion" {
+  name = "RoboShopDevBastion"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    {
+        Name = "RoboShopDevBastion"
+    },
+    local.common_tags
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "bastion" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# Create the instance profile
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${var.project}-${var.environment}-bastion"
+  role = aws_iam_role.bastion.name
 }
 
 /* resource "aws_iam_instance_profile" "bastion" {
