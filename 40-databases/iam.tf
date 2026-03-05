c1b29bda6b5bd9eee33d5926122078d554cd7711
Sivakumar Reddy M <info@joindevops.com>
2026-03-05 20:45:51 +0530

    terraform

---
+++ 40-databases/iam.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "mysql" {
+  name = local.mysql_iam_role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-${var.environment}-mysql"
+  })
+}
+
+resource "aws_iam_policy" "mysql" {
+  name        = local.mysql_iam_role_name
+  description = "Allow mysql to read root password from SSM Parameter Store"
+  policy      = templatefile("policies/mysql.json", {
+    environment = var.environment
+  })
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-${var.environment}-mysql"
+  })
+}
+
+# Attach Policy to Role
+resource "aws_iam_role_policy_attachment" "mysql" {
+  role       = aws_iam_role.mysql.name
+  policy_arn = aws_iam_policy.mysql.arn
+}
+
+# Instance Profile
+resource "aws_iam_instance_profile" "mysql" {
+  name = "${var.project}-${var.environment}-mysql"
+  role = aws_iam_role.mysql.name
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-${var.environment}-mysql"
+  })
+}
--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -5,6 +5,10 @@ locals {
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
     rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
     database_subnet_id = split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
+    mysql_iam_role_name = join("", [
+        for part in split("-", "${var.project}-${var.environment}-mysql") :
+        title(part)
+    ])
     /* ssh_credentials = jsondecode(
         data.aws_secretsmanager_secret_version.roboshop.secret_string
     ) */
