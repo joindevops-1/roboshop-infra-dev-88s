7d89af5e639f8cf11c992402a4cdbe6a12b1c36a
Sivakumar Reddy M <info@joindevops.com>
2026-03-16 18:40:50 +0530

    terraform

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -6,5 +6,17 @@ variable "components" {
         user = {
             rule_priority = 20
         }
+        cart = {
+            rule_priority = 30
+        }
+        shipping = {
+            rule_priority = 40
+        }
+        payment = {
+            rule_priority = 50
+        }
+        frontend = {
+            rule_priority = 10
+        }
     }
 }
